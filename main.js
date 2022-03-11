8d6430e1125ce03bd7b31e99da06136f53568c0f
aid417 <55597677+aid417@users.noreply.github.com>
2022-03-10 14:22:10 -1000

    fixed corner first move error

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ $(() => {
   let o_values = [];
   let game_won = false;
   let game_winner = null;
+  let taken_squares = [];
+
+  var turnCount = 0;  
   const winning_combos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -30,7 +33,10 @@ $(() => {
     const newXO = document.createElement("span");
     if (xo_val == true) {
       newXO.innerHTML = "X";
+      turnCount++;
+      console.log(turnCount)
       x_values.push(parseInt(event.target.id));
+      taken_squares.push(parseInt(event.target.id))
       const squareDiv = document.getElementById(event.target.id);
       squareDiv.classList.add("XO");
       squareDiv.appendChild(newXO);
@@ -38,7 +44,7 @@ $(() => {
       setTimeout(() => { check_winners(x_values, "X") }, 500);
       // check_winners(x_values, "X")
       if(game_won == false){
-        setTimeout(() => { random_move() }, 500);
+        setTimeout(() => { computerTurn() }, 500);
       }
      
       
@@ -52,7 +58,7 @@ $(() => {
       squareDiv.appendChild(newXO);
       remove_square(parseInt(event.target.id))
       check_winners(o_values, "O")
-      random_move()
+      computerTurn()
       xo_val = !xo_val;
      
     }
@@ -99,16 +105,160 @@ $(() => {
     
 
   }
-  function random_move(){
-    arr_index = Math.floor(Math.random() * available_squares.length);
+  // function random_move(){
+  //   arr_index = Math.floor(Math.random() * available_squares.length);
+  //   const newXO = document.createElement("span");
+  //   newXO.innerHTML = "O";
+  //   o_values.push(available_squares[arr_index]);
+  //   const squareDiv = document.getElementById(available_squares[arr_index]);
+  //   squareDiv.classList.add("XO");
+  //   squareDiv.appendChild(newXO);
+  //   remove_square(available_squares[arr_index])
+  //   setTimeout(() => { check_winners(o_values, "O") }, 500);
+  //   // xo_val = !xo_val;
+  // }
+  function computer_square(square_id){
     const newXO = document.createElement("span");
-    newXO.innerHTML = "O";
-    o_values.push(available_squares[arr_index]);
-    const squareDiv = document.getElementById(available_squares[arr_index]);
-    squareDiv.classList.add("XO");
-    squareDiv.appendChild(newXO);
-    remove_square(available_squares[arr_index])
-    setTimeout(() => { check_winners(o_values, "O") }, 500);
-    // xo_val = !xo_val;
+      newXO.innerHTML = "O";
+      o_values.push(square_id);
+      const squareDiv = document.getElementById(square_id);
+      squareDiv.classList.add("XO");
+      squareDiv.appendChild(newXO);
+      
+      remove_square(square_id)
+      taken_squares.push(square_id)
+     
+      setTimeout(() => { check_winners(o_values, "O") }, 500);
   }
+  function computerTurn(){
+    let next_move = null
+
+    if(turnCount == 1){
+      next_move = computeFirstMove();
+      console.log(next_move)
+    }
+    if (next_move == null){
+      next_move = computeFinishingMove();
+    }
+
+    if (next_move == null) {
+      next_move = computeSavingMove();
+      console.log(next_move)
+    }
+    if (next_move == null)
+      next_move = predictTrappingMove();
+    
+    if (next_move == null) {
+      next_move = computeRandomMove();
+    }
+
+   
+    computer_square(next_move)
+  }
+  function computeFirstMove(){
+    
+    var playedMove = taken_squares[0]
+    
+    var edgeMoves = [1, 3, 5, 7];
+    var cornerMoves = [0, 2, 6, 8];
+    var centerMove = [4];
+  
+    corner_move = (cornerMoves.find(m => m == playedMove))
+    
+    if(edgeMoves.find(m => m == playedMove)){
+    
+      return edgeMoveResponse(playedMove);
+    }
+      
+   if(corner_move == playedMove){
+      
+      move = 4
+      return move;
+    }
+     
+    if(centerMove.find(m => m == playedMove)){
+      return cornerMoves[Math.floor(Math.random()*cornerMoves.length)];
+    }
+
+      
+  }
+
+  function edgeMoveResponse(playedMove){
+  
+    if(playedMove == 1) 
+        return 0;
+    else if (playedMove == 3) 
+        return 6;
+    else if (playedMove == 5) 
+        return 2;
+    else if(playedMove == 7) 
+        return 6;
+}
+function check_possible_win(squares_taken){
+  
+    for(const combination of winning_combos){
+        let winner = 0;
+       for(let i = 0; i < combination.length; i++){
+            for(let j = 0; j < squares_taken.length; j++){
+                if(combination[i] == squares_taken[j]){
+                    winner += 1;
+                    if(winner == 3){
+                      return true;
+                      
+                    }
+                }
+            }
+       }
+    }
+  
+
+}
+function computeFinishingMove(){
+    remainingMoves = available_squares
+   
+
+    if(o_values.length >= 2){
+        for(square in available_squares){
+          o_values.push(square)
+          winning_move = check_possible_win(o_values)
+          if(winning_move == true){
+              o_values.pop()
+              return square
+          }
+          else{
+            o_values.pop()
+          }
+        }
+        return null;
+    }
+    else{
+        return null;
+    }
+}
+function computeSavingMove(){
+  remainingMoves = available_squares
+  console.log(available_squares)
+    if(x_values.length >= 2){
+        for(let i = 0; i < available_squares.length; i ++){
+          square = available_squares[i]
+        
+          x_values.push(parseInt(square))
+          winning_move = check_possible_win(x_values)
+    
+          if(winning_move == true){
+              x_values.pop()
+           
+              return square
+          }
+          else{
+            x_values.pop()
+       
+          }
+        }
+        return null;
+    }
+    else{
+        return null;
+    }
+}
 });
